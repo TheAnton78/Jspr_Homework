4cfcf943bff90e365c809774f9429a94d7b2642a
Shaa test <shvyrev.anton78@gmail.com>
2024-03-19 21:16:58 +0500

    Add hadlers

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+        public static void main(String[] args){
+            final var server = new Server();
+
+            // добавление хендлеров (обработчиков)
+            server.addHandler("GET", "/messages", new Handler() {
+                public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                    final var filePath = Path.of(".","public", "response");
+                    final var mimeType = Files.probeContentType(filePath);
+                    responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + Files.size(filePath) + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    Files.copy(filePath,responseStream);
+                    responseStream.flush();
+                }
+            });
+            server.addHandler("POST", "/messages", new Handler() {
+                public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                    final var filePath = Path.of(".","public", "postResponse");
+                    final var mimeType = Files.probeContentType(filePath);
+                    responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + Files.size(filePath) + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    Files.copy(filePath,responseStream);
+                    responseStream.flush();
+                }
+            });
+
+            Server.listen(9998);
+        }
+    }
+
